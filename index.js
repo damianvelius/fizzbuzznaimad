f385d2658c623170505b3a40a0d8a22912444554
Damian Perez Martinez <damianpm93@gmail.com>
2025-08-05 20:19:18 -0300

    Arreglo del bug del timeOut ya que el scope no llegaba por lo que se le puso un IIFE

--- index.js
+++ index.js
@@ -10,28 +10,29 @@ function generateFizz(initial, valueEnd) {
 
   let delay = 0;
 
-  for (initial; initial <= valueEnd; initial++) {
-    setTimeout(() => {
-      if (initial % 3 === 0 && initial % 5 === 0) {
-        texto = 'FIZZBUZZ';
-      } else if (initial % 3 === 0) {
-        texto = 'FIZZ';
-      } else if (initial % 5 === 0) {
-        texto = 'BUZZ';
-      } else {
-        texto = initial;
-      }
+  for (let i = initial; i <= valueEnd; i++) {
+    (function (num) {
+      setTimeout(() => {
+        if (i % 3 === 0 && i % 5 === 0) {
+          texto = 'FIZZBUZZ';
+        } else if (i % 3 === 0) {
+          texto = 'FIZZ';
+        } else if (i % 5 === 0) {
+          texto = 'BUZZ';
+        } else {
+          texto = i;
+        }
 
-      const box = document.createElement('div');
+        const box = document.createElement('div');
 
-      if (texto === 'FIZZBUZZ') box.className = 'fizzbuzz';
-      else if (texto === 'FIZZ') box.className = 'fizz';
-      else if (texto === 'BUZZ') box.className = 'buzz';
+        if (texto === 'FIZZBUZZ') box.className = 'fizzbuzz';
+        else if (texto === 'FIZZ') box.className = 'fizz';
+        else if (texto === 'BUZZ') box.className = 'buzz';
 
-      box.textContent = texto;
-      contenedor.appendChild(box);
-      initial++;
-    }, delay);
+        box.textContent = texto;
+        contenedor.appendChild(box);
+      }, delay);
+    })(i);
     delay += 100; // 100ms entre cada número
   }
 }
@@ -70,4 +71,12 @@ playButton.addEventListener('click', () => {
 lo habia armado con while pero con el setTimeout habia conflictos
 Porque for (let i = ... ) crea una nueva copia de i en cada iteración, y setTimeout la captura correctamente.
 En cambio, while usa siempre la misma initial, y para cuando los setTimeout se ejecutan... ya cambió.
+
+ademas el timeOut tarda demasiado por lo que tengo que usar un
+IIFE = Immediately Invoked Function Expression.
+
+Es una función que declarás y llamás al instante.
+
+Se usa para crear un scope propio, separado del contexto exterior(porque el scope del i no se guarda a tiempo y termina siendo un 101 constante en el I
+Porque cuando el bucle itera rápido, los setTimeout programados no capturan el valor de i en el momento, sino que apuntan a la misma variable i, que al final del bucle vale 101.
 */
